4f4b615fc205639a35f897c96f42628a7396aa32
Stevens <u20201e735@upc.edu.pe>
2023-10-18 21:37:01 -0500

    refactor: changing name for bounded context

--- src/main/java/pe/edu/upc/schedule/stevensacha/domain/model/entities/Teacher.java
+++ src/main/java/pe/edu/upc/schedule/stevensacha/domain/model/entities/Teacher.java
@@ -1,4 +1,4 @@
-package pe.edu.upc.schedule.stevens_acha.domain.model.entities;
+package pe.edu.upc.schedule.stevensacha.domain.model.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
